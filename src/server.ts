9143940ef2242dbcad3d0e651cc17ef9698b8975
Vladimir Bolshakov <andersdeath@yandex.ru>
2023-09-05 18:16:48 +0400

    minor fix

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import { express } from "express";
+import express from "express";
 import { countLanguagesAndSize, fetchData } from "./engine";
 import { DisplayResultsParams } from "./interfaces";
 
